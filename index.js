ab0565f39547f67fec2563f248a1e3a40a3be0ee
mrmuli <jayjaymuli00@gmail.com>
2019-08-28 18:41:16 +0300

    deprecate need for client

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ app.get('/api/getList', (req,res) => {
     console.log('Sent list of Mangoes');
 });
 
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
-
 const port = process.env.PORT || 5000;
 app.listen(port);
 
